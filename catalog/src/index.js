cd63801fd0b98cc196902b5dc3424031c167fc0a
Greg Chamberlain <gregchamberlain94@gmail.com>
2016-09-28 14:30:27 -0700

    Rebundled

--- catalog/src/index.js
+++ catalog/src/index.js
@@ -1,6 +1,7 @@
 import { Row, Column , Text, Title } from 'react-dnd-layout';
 import Link from './link';
+import RichText from './rich_text.jsx';
 
 export default {
-  Row, Column, Text, Title, Link
+  Row, Column, Text, Title, Link, RichText
 };
